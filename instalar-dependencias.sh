2a048f27b6434ba2b9f4e232c71b85fae112528f
Clovis Reis <clovis.fmu@live.com>
2024-06-14 22:33:02 -0300

    ajuste docker installer

--- instalar-dependencias.sh
+++ instalar-dependencias.sh
@@ -1,21 +1,21 @@
-echo "Iniciando atualizacoes..."
+#echo "Iniciando atualizacoes..."
 
-apt-get update && apt-get -y upgrade
+#apt-get update && apt-get -y upgrade
 echo "Atualizacoes finalizadas!"
 echo "************************************************************"
 echo "Instalando dependencias do projeto..."
 echo "************************* Docker ***************************"
-apt-get install ca-certificates curl -y
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-echo "************************** MySql ****************************"
-apt install mysql-client-8.0 -y
-apt install jupyter -y
+apt remove docker docker-engine docker.io containerd runc -y
+apt update
+apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+-fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+apt update
+apt install docker-ce docker-ce-cli containerd.io -y
+systemctl start docker
+docker run hello-world
+#echo "************************** MySql ****************************"
+#apt install mysql-client-8.0 -y
+#apt install jupyter -y
 
 echo "Atualizacoes concluidas!"
