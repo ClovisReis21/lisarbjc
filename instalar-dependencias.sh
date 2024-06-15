754c1557a279bf34f99a66bc7a702bc3c16673d5
Clovis Reis <clovis.fmu@live.com>
2024-06-14 23:51:58 -0300

    ajustes

--- instalar-dependencias.sh
+++ instalar-dependencias.sh
@@ -1,9 +1,11 @@
 #echo "Iniciando atualizacoes..."
 
-#apt-get update && apt-get -y upgrade
+apt-get update && apt-get -y upgrade
 echo "Atualizacoes finalizadas!"
 echo "************************************************************"
 echo "Instalando dependencias do projeto..."
+echo "************************** Java ****************************"
+apt install openjdk-11-jre-headless -y
 echo "************************* Docker ***************************"
 apt remove docker docker-engine docker.io containerd runc -y
 apt update
@@ -14,8 +16,9 @@ apt update
 apt install docker-ce docker-ce-cli containerd.io -y
 systemctl start docker
 docker run hello-world
-#echo "************************** MySql ****************************"
-#apt install mysql-client-8.0 -y
-#apt install jupyter -y
+echo "************************** MySql ****************************"
+apt install mysql-client-8.0 -y
+apt install python3-pip
+apt install jupyter -y
 
 echo "Atualizacoes concluidas!"
